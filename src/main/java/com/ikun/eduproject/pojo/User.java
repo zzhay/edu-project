377bc12a754b394ef778542ce5c08ca1f545d33a
zzhay <737766080@qq.com>
2023-07-26 15:35:00 +0800

    User

--- src/main/java/com/ikun/eduproject/pojo/User.java
+++ src/main/java/com/ikun/eduproject/pojo/User.java
@@ -1,8 +1,49 @@
 package com.ikun.eduproject.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
 /**
  * @Author zzhay
  * @Date 2023/7/26/026
  */
-public class User {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(value = "用户对象",description = "用户信息")
+public class User implements Serializable {
+
+    @ApiModelProperty(dataType = "Integer",required = false)
+    private int id;
+
+    @ApiModelProperty(dataType = "String",required = true,value = "用户名")
+    private String username;
+
+    @ApiModelProperty(dataType = "String",required = true,value = "密码")
+    private String password;
+
+    @ApiModelProperty(dataType = "int",required = true,value = "角色（（0：管理员，1：老师，2：学生））")
+    private int role;
+
+    @ApiModelProperty(dataType = "Integer",required = true,value = "电话")
+    private Integer phone;
+
+    @ApiModelProperty(dataType = "String",required = true,value = "邮件")
+    private String email;
+
+    @ApiModelProperty(dataType = "int",required = true,value = "状态（0：锁定，1：未锁定）")
+    private int statu;
+
+    @ApiModelProperty(dataType = "Timestamp",required = true,value = "创建时间")
+    private Timestamp creatTime;
+
+    @ApiModelProperty(dataType = "Timestamp",required = true,value = "更新时间")
+    private Timestamp updateTime;
+
 }
