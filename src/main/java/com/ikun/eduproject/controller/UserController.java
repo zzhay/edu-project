7205c0e41caaeddec7292234917059d5fbfd6b99
zzhay <737766080@qq.com>
2023-07-26 15:11:24 +0800

    初始化

---
+++ src/main/java/com/ikun/eduproject/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.ikun.eduproject.controller;
+
+/**
+ * @Author zzhay
+ * @Date 2023/7/26/026
+ */
+public class UserController {
+}
---
+++ src/main/java/com/ikun/eduproject/utils/MD5Utils.java
@@ -0,0 +1,8 @@
+package com.ikun.eduproject.utils;
+
+/**
+ * @Author zzhay
+ * @Date 2023/7/26/026
+ */
+public class MD5Utils {
+}
